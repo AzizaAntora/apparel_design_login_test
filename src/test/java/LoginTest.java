a7608af8250ec1f5f0fa3a235b4ee750520ce1e4
Aziza Sultana <azizaantora@gmail.com>
2023-12-25 02:53:18 +0600

    AppearlLoginTest

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,52 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginTest {
+
+	@Test
+	public void loginTest() {
+
+		// Initialize ChromeDriver
+		WebDriver driver = new ChromeDriver();
+
+		// Navigate to the login page
+		driver.get("https://automationexercise.com/login");
+		driver.manage().window().maximize();
+
+		// Locate the user name/email and password fields
+		WebElement emailField = driver.findElement(By.name("email"));
+		WebElement passwordField = driver.findElement(By.name("password"));
+
+		// Enter user name and password
+		emailField.sendKeys("validusername@example.com");
+		passwordField.sendKeys("password123");
+
+		// Locate and click the login button
+		WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/button"));
+		loginButton.click();
+
+		// Wait for the dashboard page to load (adjust as needed)
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.urlToBe("https://automationexercise.com/"));
+
+		// Verification - Check if the dashboard page is loaded
+		String expectedURL = "https://automationexercise.com/";
+		String currentURL = driver.getCurrentUrl();
+
+		Assert.assertEquals(currentURL, expectedURL);
+
+		// Close the browser
+		driver.quit();
+	}
+}
